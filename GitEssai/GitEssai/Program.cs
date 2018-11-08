0d273aa12cab5720d81d5375452135736422ea23
D.R <denyski@gmail.com>
2018-11-08 14:52:01 +0100

    suite

--- GitEssai/GitEssai/Program.cs
+++ GitEssai/GitEssai/Program.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Light;
 namespace GitEssai
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Personne t = new Personne();
+            Console.WriteLine();
+
+
+            Console.ReadKey();
+
         }
     }
 }
