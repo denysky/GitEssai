7fb4c88740f957360dc612eb7e5a501ce1d850c0
denysky <34537140+denysky@users.noreply.github.com>
2018-11-08 11:49:29 +0100

    Add files via upload

---
+++ GitEssai/ClassLibrary1/Personne.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Light
+
+{
+    public class Personne
+    {
+        public string Name{ get; set; }
+        public Personne(string name)
+        {
+            Name = name;
+        }
+        public Personne()
+        {
+            Name = "Toto";
+        }
+    }
+}
